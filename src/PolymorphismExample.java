155d9182b5162e4ab4af809a4f856390f18ae112
Artem <medvynskyi.artem@gmail.com>
2019-06-13 14:29:30 +0300

    animal

---
+++ src/PolymorphismExample.java
@@ -0,0 +1,11 @@
+public class PolymorphismExample {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.makeSound();
+        Dog dog = new Dog();
+        dog.makeSound();
+        animal = new Cat();
+        animal.makeSound();
+    }
+}
